eea09cd2c7a0658be6e81b259655a4e918e10779
wladyslaw <wladyslaw@jellinek.pl>
2022-05-30 14:48:25 +0200

    removed unnecessary parameters

--- MessageBuilder.php
+++ MessageBuilder.php
@@ -9,24 +9,9 @@ use Symfony\Component\Mime\Address;
 class MessageBuilder
 {
     /**
-     * @var string|null
-     */
-    private $subject;
-
-    /**
-     * @var array
-     */
-    private $to = [];
-
-    /**
      * @var string
      */
     private $from;
-
-    /**
-     * @var array
-     */
-    private $replyTo = [];
 
     public function __construct(ParameterBagInterface $parameterBag)
     {
@@ -37,45 +22,12 @@ class MessageBuilder
         $this->from = new Address($parameterBag->get('postmaster_email'));
     }
 
-    public function setSubject(string $subject)
+    public function getMessage(string $subject, ?array $data, string $template): TemplatedEmail
     {
-        $this->subject = $subject;
-    }
-
-    public function addTo(string $email, string $name = ''): self
-    {
-        $this->to[] = new Address($email, $name);
-
-        return $this;
-    }
-
-    public function addReplyTo(string $email, string $name): self
-    {
-        $this->replyTo[] = new Address($email, $name);
-
-        return $this;
-    }
-
-    public function getMessage(MessageInterface $data, string $template): TemplatedEmail
-    {
-        if (count($this->to) === 0) {
-            throw new \Exception('No recipient');
-        }
-        if (is_null($this->subject)) {
-            throw new \Exception('No subject');
-        }
-        if (count($this->replyTo) === 0) {
-            throw new \Exception('No reply to');
-        }
-
         return (new TemplatedEmail())
-            ->subject($this->subject)
+            ->subject($subject)
             ->from($this->from)
-            ->to(...$this->to)
-            ->replyTo(...$this->replyTo)
             ->htmlTemplate($template)
-            ->context([
-                'data' => $data
-            ]);
+            ->context($data);
     }
 }
